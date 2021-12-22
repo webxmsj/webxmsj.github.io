38abd5b5dc8cf1e4ad886e4048e2607afaae95a6
webxmsj <951216388@qq.com>
2021-12-22 23:14:04 +0800

    百度主动推送token修改

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://bfrontend.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://bfrontend.com&token=7QqHAy7DFwWIL7QK"
 
 rm -rf urls.txt # 删除文件
